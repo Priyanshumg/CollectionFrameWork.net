18979e689c15c4913516191e53c241c3c32e3774
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-05 12:13:15 +0530

    Add: Added functionality for adding Elements in an array

--- LinkedListOperations.cs
+++ LinkedListOperations.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CollectionFrameWorks
@@ -9,5 +11,16 @@ namespace CollectionFrameWorks
     internal class LinkedListOperations
     {
         public static LinkedList<string> myLinkedList = new LinkedList<string>();
+
+        public static void AddElementsInLinkedList(String[] ArrayUserData)
+        {
+            Console.WriteLine("Adding Elements in LinkedList");
+            foreach (string item in ArrayUserData)
+            {
+                myLinkedList.AddLast(item);
+            }
+            Console.WriteLine("Added Elements SuccessFully");
+            Console.WriteLine();
+        }
     }
 }
