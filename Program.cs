b9787a91401e43b6742d8edc5aaee8cd5580bed3
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-05 13:01:24 +0530

    [Priyanshu] Comment: Added comments

--- Program.cs
+++ Program.cs
@@ -10,12 +10,25 @@ namespace CollectionFrameWorks
     {
         static void Main(string[] args)
         {
+            // Array For Inputs
             String[] UsersDataArray = { "Arun", "Garv", "Sunil", "Prince", "Aish" ,"Ankit", "Esha", "Namit", "Manikanta", "Dhanush", "Shovik", "Prakhar" };
+            
+            // Add Elements in LinkedList
             LinkedListOperations.AddElementsInLinkedList(UsersDataArray);
+            
+            // Print All Elements in LinkedList
             LinkedListOperations.PrintLinkedList();
+            
+            // Remove First Element In LinkedList
             LinkedListOperations.RemoveFirstElement();
+            
+            // Remove Last Element in LinkedList
             LinkedListOperations.RemoveLastElement();
+
+            // Remove Element By Passing Value
             LinkedListOperations.RemoveByValue("Aish");
+            
+            // Removing All the elements in LinkedList
             LinkedListOperations.RemoveAllElementsLinkedList();
             Console.ReadLine();
         }   
