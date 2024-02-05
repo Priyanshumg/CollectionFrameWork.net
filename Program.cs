1f402eb9565f7f4b921c7b90a860b0a63dcbaba6
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-05 13:26:24 +0530

    [Priyanshu] Create: Created a method to perform only LinkedList Operations

--- Program.cs
+++ Program.cs
@@ -8,28 +8,33 @@ namespace CollectionFrameWorks
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static String[] UsersDataArray = { "Arun", "Garv", "Sunil", "Prince", "Aish", "Ankit", "Esha", "Namit", "Manikanta", "Dhanush", "Shovik", "Prakhar" };
+
+        private static void LinkedList_Operations()
         {
-            // Array For Inputs
-            String[] UsersDataArray = { "Arun", "Garv", "Sunil", "Prince", "Aish" ,"Ankit", "Esha", "Namit", "Manikanta", "Dhanush", "Shovik", "Prakhar" };
-            
             // Add Elements in LinkedList
             LinkedListOperations.AddElementsInLinkedList(UsersDataArray);
-            
+
             // Print All Elements in LinkedList
             LinkedListOperations.PrintLinkedList();
-            
+
             // Remove First Element In LinkedList
             LinkedListOperations.RemoveFirstElement();
-            
+
             // Remove Last Element in LinkedList
             LinkedListOperations.RemoveLastElement();
 
             // Remove Element By Passing Value
             LinkedListOperations.RemoveByValue("Aish");
-            
+
             // Removing All the elements in LinkedList
             LinkedListOperations.RemoveAllElementsLinkedList();
+        }
+        static void Main(string[] args)
+        {
+            // Array For Inputs
+
+            LinkedList_Operations();
             Console.ReadLine();
         }   
     }
