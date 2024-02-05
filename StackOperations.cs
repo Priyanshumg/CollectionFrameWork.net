8fcdce5cf429d1078401e3a43bbdd70e4daf3159
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-05 16:54:10 +0530

    [Priyanshu] Add: Added A new File StackOperation.cs with a new stack object created

---
+++ StackOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionFrameWorks
+{
+    class StackOperations
+    {
+        public static Stack<string> newStack = new Stack<string>();
+    }
+}
