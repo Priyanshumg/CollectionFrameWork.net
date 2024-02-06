7cfed407339f86ed90967719717f78d12d26aa43
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-06 10:10:09 +0530

    [Priyanshu] Add: Added method to addElementsInAQueue

--- QueueOperations.cs
+++ QueueOperations.cs
@@ -9,6 +9,18 @@ namespace CollectionFrameWorks
 {
     class QueueOperations
     {
-        public static Queue myQueue = new Queue();
+        public static Queue newQueue= new Queue();
+
+        static void AddElementeInQueue(string[] ArrayUserData)
+        {
+            Console.WriteLine("Adding Elements to the Queue");
+            Console.Write("{ ");
+            foreach (string elements in ArrayUserData)
+            {
+                newQueue.Enqueue(elements + ", ");
+            }
+            Console.WriteLine(", Null }");
+            Console.WriteLine("Added Elements to the Queue Successfully");
+        }
     }
 }
