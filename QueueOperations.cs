826be18d627a43477603d6f9c9a8676caddf5b25
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-06 10:00:50 +0530

    [Priyanshu] Create: Created a new Queue Object

--- QueueOperations.cs
+++ QueueOperations.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace CollectionFrameWorks
 {
-    internal class QueueOperations
+    class QueueOperations
     {
-
+        public static Queue myQueue = new Queue();
     }
 }
